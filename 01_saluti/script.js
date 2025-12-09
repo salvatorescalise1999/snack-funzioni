2ef26d8b9b4019328e5135e177a58bd473b39a37
Salvatore Scalise <salvatorescalise1999@gmail.com>
2025-12-09 15:53:40 +0100

    feat: aggiunta funzione che restituisce un saluto con il nome fornito

--- 01_saluti/script.js
+++ 01_saluti/script.js
@@ -5,8 +5,13 @@ const userName = 'Mario';
 
 // Dichiara la funzione qui.
 
+function sayHello(name) {
+  return "Ciao " + name;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
+
+console.log(sayHello(userName));
 
 
 
