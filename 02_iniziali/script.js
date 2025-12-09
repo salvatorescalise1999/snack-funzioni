4186504f4da06521b0ca1503aa2e693abbaeb32b
Salvatore Scalise <salvatorescalise1999@gmail.com>
2025-12-09 16:04:08 +0100

    feat: aggiunta funzione prendiIniziali che restituisce le iniziali dei nomi forniti

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -5,9 +5,16 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 // Dichiara la funzione qui.
 
+function prendiIniziali(listaNomi) {
+    const iniziali = [];         // Creo un nuovo array vuoto dove inserirò le iniziali
+    for (let i = 0; i < listaNomi.length; i++) {
+        iniziali.push(listaNomi[i][0]);          // Prendo la prima lettera del nome e la aggiungo all'array iniziali
+    }
+    return iniziali;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(prendiIniziali(names));
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
